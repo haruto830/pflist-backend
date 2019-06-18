8bde6700f179351728043b2d394d1b606cbf4e2f
h.maeyama <haruto8300@gmail.com>
2019-06-18 17:28:02 +0900

    fix create_table.sql

--- scripts/mysql_init/create-tables.sql
+++ scripts/mysql_init/create-tables.sql
@@ -1,10 +1,11 @@
-CREATE TABLE IF NOT EXISTS `USERS`
+-- userテーブル
+create table if not exists `users`
 (
-`USER_ID`	    VARCHAR(255) NOT NULL,
-`USER_NAME`	    VARCHAR(255) NOT NULL,
-`UID`           VARCHAR(255) NOT NULL,
-`INSERT_DATA`   DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-`UPDATE_DATE`	DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-`DELETE_FLAG`	TINYINT(1) NOT NULL,
-PRIMARY KEY (`USER_ID`)
-) DEFAULT CHARSET=UTF8 COLLATE=UTF8_BIN;
+`user_id`	    varchar(255) not null,
+`user_name`	    varchar(255) not null,
+`uid`           varchar(255) not null,
+`insert_data`   datetime not null default current_timestamp,
+`update_date`	datetime not null default current_timestamp,
+`delete_flag`	tinyint(1) not null,
+primary key (`user_id`)
+) default charset=utf8 collate=utf8_bin;
